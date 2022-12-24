069a108ca0cb80b210e84fe90b90296b2bf13301
AbramkinApps <eugemailbox@gmail.com>
2022-12-24 17:57:32 +0300

    add room

--- app/src/main/java/com/abramkinapps/android/newsapp/MainActivity.kt
+++ app/src/main/java/com/abramkinapps/android/newsapp/MainActivity.kt
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.PersistableBundle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.abramkinapps.android.newsapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
@@ -16,17 +24,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.fragment_splash)
-        Handler(Looper.myLooper()!!).postDelayed(
-            {
-                setContentView(mBinding?.root)
-                bottom_nav_menu.setupWithNavController(
-                    navController = nav_host_fragment.findNavController()
-                )
-            }, 5000
-        )
+//        Handler(Looper.myLooper()!!).postDelayed(
+//            {
+//                setContentView(mBinding?.root)
+//                bottom_nav_menu.setupWithNavController(
+//                    navController = nav_host_fragment.findNavController()
+//                )
+//            }, 5000
+//        )
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(5000)
+            _binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(mBinding?.root)
+            bottom_nav_menu.setupWithNavController(
+                navController = nav_host_fragment.findNavController()
+            )
+
+        }
+
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
---
+++ app/src/main/java/com/abramkinapps/android/newsapp/di/NewsApp.kt
@@ -0,0 +1,8 @@
+package com.abramkinapps.android.newsapp.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsApp: Application() {
+}
