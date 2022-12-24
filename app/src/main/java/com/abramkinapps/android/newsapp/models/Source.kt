87ce33aa98f6d2b8b6f19b4c7d5434d9927c77cd
AbramkinApps <eugemailbox@gmail.com>
2022-12-24 15:05:20 +0300

    add retrofit

---
+++ app/src/main/java/com/abramkinapps/android/newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package com.abramkinapps.android.newsapp.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/abramkinapps/android/newsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.abramkinapps.android.newsapp.utils
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "5c7543b7fd9c4d6b8be8ce80f8cdf877"
+    }
+}
